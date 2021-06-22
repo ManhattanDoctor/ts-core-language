16ce19baad882c96157e289275d0edb19963fdf2
Renat Gubaev <>
2021-06-22 13:12:08 +0300

    no message

---
+++ src/error/TemplateError.ts
@@ -0,0 +1,39 @@
+import { ExtendedError } from '@ts-core/common/error';
+import * as _ from 'lodash';
+
+export class TemplateError<U = any, V = number> extends ExtendedError<U, V> {
+    // --------------------------------------------------------------------------
+    //
+    // 	Static Methods
+    //
+    // --------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
+    //
+    // 	Properties
+    //
+    // --------------------------------------------------------------------------
+
+    protected template: string;
+
+    // --------------------------------------------------------------------------
+    //
+    // 	Constructor
+    //
+    // --------------------------------------------------------------------------
+
+    constructor(message: string, code?: V, details?: U, isFatal?: boolean) {
+        super(message, code, details, isFatal);
+    }
+
+    // --------------------------------------------------------------------------
+    //
+    // 	Public Message
+    //
+    // --------------------------------------------------------------------------
+
+    public translate(locale?: string): void {
+        if (_.isNil(this.template)) {
+            return;
+        }
+    }
+}

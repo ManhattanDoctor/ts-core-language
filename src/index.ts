f77ad9ec128f35fe6bed1320ec19da6ca7b96c4a
Renat Gubaev <>
2021-07-30 17:38:36 +0300

    auto commit

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,3 @@ export * from './ILanguageLoader';
 export * from './ILanguageTranslator';
 export * from './Language';
 export * from './LanguageLocale';
-
-export * from './loader/LanguageFileLoader';
-export * from './translator/LanguageTranslator';

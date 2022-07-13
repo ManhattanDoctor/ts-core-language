17e752d36ec78fe2d1f1728536d6c10058ec25bd
Renat Gubaev <renat.gubaev@gmail.com>
2022-07-13 18:14:20 +0300

    no message

---
+++ src/public-api.ts
@@ -0,0 +1,8 @@
+export * from './ILanguageLoader';
+export * from './ILanguageTranslator';
+export * from './Language';
+export * from './LanguageLocale';
+//
+export * from './error/TemplateError';
+export * from './loader/LanguageFileLoader';
+export * from './loader/LanguageUrlLoader';

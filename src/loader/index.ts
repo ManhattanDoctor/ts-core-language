80f5c35da0ad079b17e38436e371b564c9a45df0
Renat Gubaev <renat.gubaev@gmail.com>
2022-08-12 21:45:29 +0300

    auto commit

--- src/loader/index.ts
+++ src/loader/index.ts
@@ -1,4 +1,6 @@
 export * from './LanguageUrlLoader';
 export * from './LanguageFileLoader';
+export * from './LanguagePreloadLoader';
 export * from './LanguageDelegateLoader';
+
 export * from './LanguageLoader';
